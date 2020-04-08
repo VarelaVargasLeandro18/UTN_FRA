9cf9724fa217567dd8c1fab41530d1926bcb59bf
VarelaVargasLeandro18 <varelavargasleandro@outlook.com.ar>
2020-04-07 21:11:24 -0300

    Creación de los prototipos de las funciones de cálculo

---
+++ tp_laboratorio_1/TP_1/src/calculos.h
@@ -0,0 +1,39 @@
+/* brief Convierte un número de tipo float a su entero más cercano.
+ * param float Número a convertir.
+ * return int Devuelve el número aproximado de tipo int.
+ */
+int Float_a_Int (float);
+
+/* brief Sumar dos enteros.
+ * param int Operando uno.
+ * param int Operando dos.
+ * return int Devuelve la suma de los operandos.
+ */
+int Suma (int, int);
+
+/* brief Restar dos enteros.
+ * param int Operando uno.
+ * param int Operando dos.
+ * return int Devuelve el resultado de la resta de los operandos.
+ */
+int Resta (int, int);
+
+/* brief Multiplica dos enteros.
+ * param int Operando uno.
+ * param int Operando dos.
+ * return int Devuelve el resultado de la multiplicación de los operandos.
+ */
+int Multiplicación (int, int);
+
+/* brief Divide dos enteros.
+ * param int Operando uno.
+ * param int Operando dos.
+ * return int Devuelve el resultado de la división de los operandos.
+ */
+int Division (int, int);
+
+/* brief Realiza el factorial de un operando entero.
+ * param int Operando.
+ * return long int Devuelve el factorial del número.
+ */
+long int Factorial (int);
